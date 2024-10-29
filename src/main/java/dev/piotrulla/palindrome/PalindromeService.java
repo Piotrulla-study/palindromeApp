2802171b017e46b44b568725794fd67b422a1926
Piotr Zych <77621271+P1otrulla@users.noreply.github.com>
2024-10-29 11:35:52 +0100

    Add regex to cleaning invalid chars

--- src/main/java/dev/piotrulla/palindrome/PalindromeService.java
+++ src/main/java/dev/piotrulla/palindrome/PalindromeService.java
@@ -4,10 +4,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.regex.Pattern;
 
 public class PalindromeService {
 
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
+    private static final Pattern ONLY_CHARS = Pattern.compile("[^a-zA-Z0-9]");
     private static final String FILE_NAME = "palindrome_{TIME-STAMP}.txt";
 
     public boolean generateFile(String[] words) {
@@ -21,7 +23,7 @@ public class PalindromeService {
     }
 
     boolean isPalindrome(String word) {
-        String cleanText = word.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        String cleanText = this.cleanText(word);
 
         return cleanText.contentEquals(new StringBuilder(cleanText).reverse());
     }
@@ -31,6 +33,7 @@ public class PalindromeService {
 
         for (String word : words) {
             word = word.trim();
+            word = this.cleanText(word);
 
             if (this.isPalindrome(word)) {
                 result.append(word).append(" - jest palindromem\n");
@@ -42,6 +45,10 @@ public class PalindromeService {
 
         return result.toString();
 
+    }
+
+    private String cleanText(String text) {
+        return text.replaceAll(ONLY_CHARS.pattern(), "").toLowerCase();
     }
 
     private String generateFileName() {
